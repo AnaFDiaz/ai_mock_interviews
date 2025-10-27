1d15cdd38fa71fc6a625938391df4dc45c7644db
AnaFDiaz <anaferds172@gmail.com>
2025-10-27 12:54:45 -0600

    restore firebase env variables configuration

--- components/firebase/client.ts
+++ components/firebase/client.ts
@@ -2,15 +2,14 @@ import { initializeApp, getApp, getApps } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
-
 const firebaseConfig = {
-  apiKey: "REDACTED",
-  authDomain: "prepwise-4f785.firebaseapp.com",
-  projectId: "prepwise-4f785",
-  storageBucket: "prepwise-4f785.firebasestorage.app",
-  messagingSenderId: "1053457186031",
-  appId: "1:1053457186031:web:0974890e21fe665371ef1a",
-  measurementId: "G-X0T075DZYV"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = !getApps.length ? initializeApp(firebaseConfig) : getApp();
